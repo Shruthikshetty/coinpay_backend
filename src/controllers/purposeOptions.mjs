754f36a520697bc15634377c39cf6b295103c3eb
shruthik shetty <shrutikshetty14@gmail.com>
2025-05-04 21:25:56 +0530

    add aditions methods for purpose options

--- src/controllers/purposeOptions.mjs
+++ src/controllers/purposeOptions.mjs
@@ -16,3 +16,45 @@ export const getPurposeOptions = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// this function is used to add a new purpose option
+export const addPurposeOption = async (req, res) => {
+  // check if the request body is empty or not
+  if (!req.body) {
+    return res.status(400).json({ message: "Request body is empty" });
+  }
+  try {
+    // create a new purpose option using the data from the request body
+    const newPurposeOption = new PurposeOption(req.body);
+    // save the new purpose option to the database
+    const savedPurposeOption = await newPurposeOption.save();
+    // return the saved purpose option in the response
+    res.status(201).json(savedPurposeOption);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//this function is used to delete a purpose option by id
+export const deletePurposeOption = async (req, res) => {
+  // check if id is present in the request params or not
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({ message: "Id is required" });
+  }
+  try {
+    // delete the purpose option from the database using the id from the request body
+    const deletedPurposeOption = await PurposeOption.findByIdAndDelete(id);
+    // check if the purpose option is found or not
+    if (!deletedPurposeOption) {
+      return res.status(404).json({ message: "Purpose option not found" });
+    }
+    // return the deleted purpose option in the response
+    res.status(200).json({
+      message: "Purpose option deleted successfully",
+      deletedPurposeOption,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/purposeOptions.mjs
+++ src/routes/purposeOptions.mjs
@@ -1,11 +1,19 @@
 /** this file congtains all the routes retaled to purpose-options */
 import { Router } from "express";
-import { getPurposeOptions } from "../controllers/purposeOptions.mjs";
+import {
+  addPurposeOption,
+  getPurposeOptions,
+  deletePurposeOption,
+} from "../controllers/purposeOptions.mjs";
 
 // initialize router
 const router = Router();
 
 // route to get all the purpose options
 router.get("/", getPurposeOptions);
+// route to add a new purpose option
+router.post("/", addPurposeOption);
+// route to delete a purpose option by id
+router.delete("/:id", deletePurposeOption);
 
 export default router;
